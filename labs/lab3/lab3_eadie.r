cd49c0457d7cf0b1e12443f96324c75c8d063a80
Gwendolyn Eadie <gwen.eadie@gmail.com>
2018-08-08 08:03:25 -0700

    homework 3 figures and R script

--- labs/lab3/lab3_eadie.r
+++ labs/lab3/lab3_eadie.r
@@ -3,12 +3,12 @@
 
 library(DAAG)
 
-##### 2.3
+####### 2.3
 
 # check the structure of the possum data
 str(possum)
 
-pdf(filename = "q2-3_hist.pdf" )
+pdf(file = "q2-3_eadie.pdf" )
 
 hist(x = possum$earconch, xlab="earconch")
 box()
@@ -26,6 +26,58 @@ plot(density(possum$earconch[possum$sex=="f"]), xlab = "earconch", main="Female 
 # plot the females' earconch distribution
 plot(density(possum$earconch[possum$sex=="m"]), xlab = "earconch", main="Male Earconch Distribution")
 
-#weird, they are both bimodal... maybe because of age?
-plot(density(na.omit(possum$age)))
+# weird, they are both bimodal... maybe because of age?
+plot(density(na.omit(possum$age)), main="age distribution")
+
+# could be it!
+
+dev.off()
+
+###### 2.5
+
+pdf(file = "q2-5_eadie.pdf")
+
+plot(formula = cbreadth ~ clength, data=cuckoohosts, ylim=c(12, 18), xlim=c(16, 24))
+points(formula= hbreadth ~ hlength, data=cuckoohosts, col="blue", pch=19)
+
+for(i in 1:12){
+  with(cuckoohosts, expr = lines(c(clength[i], hlength[i]), c(cbreadth[i], hbreadth[i])))
+  
+}
+
+legend("topleft", legend = c("cuckoo", "host", ))
+dev.off()
+
+
+###### 2.10
+
+library(MASS)
+
+with( Animals, cor(brain, body) )
+
+with( Animals, cor(log(brain), log(body)) )
+
+# note that the spearman rank coefficient gives the same answer whether it is logged or not, as long as the data have a monotonic function? i.e. the ranks don't change under the transformation
+with( Animals, cor(log(brain), log(body), method="spearman" ))
+with( Animals, cor(brain, body, method="spearman" ))
+
+with(Animals, plot(log(brain), log(body)))
+
+# pearson correlation coefficient is the covariance divided by variance squared
+
+
+###### 2.13 
+str(galaxies)
+
+
+plot(density(galaxies))
+
+# or make a nicer plot from the example in ?galaxies
+gal <- galaxies/1000
+c(width.SJ(gal, method = "dpi"), width.SJ(gal))
+plot(x = c(0, 40), y = c(0, 0.3), type = "n", bty = "l",
+     xlab = "velocity of galaxy (1000km/s)", ylab = "density", main="example from galaxies help file")
+rug(gal)
+lines(density(gal, width = 3.25, n = 200), lty = 1)
+lines(density(gal, width = 2.56, n = 200), lty = 3)
 
