de14a6ae2b05b7b5378ddbfd6caaa3f4190ee4cf
Gwendolyn Eadie <gwen.eadie@gmail.com>
2018-08-13 22:54:16 -0700

    finished markov chain question, added plot for extra question

--- labs/lab4/Lab4-eadie.r
+++ labs/lab4/Lab4-eadie.r
@@ -94,10 +94,10 @@ ratdat = c(87, 53, 72, 90, 78, 85, 83)
 sampmean = mean(ratdat)
 sampvar = var(ratdat)
 
-# I would say that the Poisson model is not apprpriate, since the sample mean and sample variance are not near equal (variance is twice the mean)
+# I would say that the Poisson model is not appropriate, since the sample mean and sample variance are not near equal (variance is twice the mean)
 
 # repeat this several times to see if the Poisson assumotion holds
-samps = as.data.frame(matrix(NA, ncol = 2, nrow = fewtimes))
+samps = as.data.frame(matrix(NA, ncol = 2, nrow = 1e3))
 colnames(samps) = c("mu", "lambda")
 
 for(i in 1:fewtimes){
@@ -109,9 +109,97 @@ for(i in 1:fewtimes){
 
 # plot the difference between the sample mean and sample variance for each simulations experiement
 pdf("q3-11_eadie.pdf")
-plot(samps$mu-samps$lambda, ylab=expression(mu-lambda), xlab="simulation")
+
+plot(samps$mu, samps$lambda, ylab=expression(mu), xlab=expression(lambda))
+
 dev.off()
 
 
 
 ###### 3.13
+
+# the transition matrix for the Markov chain
+Tmat = matrix(c(0.6, 0.2, 0.2, 0.2, 0.4, 0.4, 0.4, 0.3, 0.3), nrow = 3, ncol=3, byrow = TRUE)
+
+# assign numbers to the states
+sun = 1
+cloud = 2
+rain = 3
+
+Markov = function(N, init, transmat, states=c(sun, cloud, rain)){
+  
+  # make empty chain
+  chain = numeric(N)
+  # current state
+  chain[1] = init
+  # number of states
+  n = length(states)
+  
+  for(i in 2:N){
+    oldstate = chain[i-1]
+    # draw a new state given the previous state
+    chain[i] = sample(x = 1:n, size = 1, prob = transmat[oldstate, ])
+  }
+  
+  chain
+  
+}
+
+chainlength = 1e3
+samplechain = Markov(N = chainlength, init=2, transmat = Tmat)
+
+sum(samplechain==1)/chainlength
+sum(samplechain==2)/chainlength
+sum(samplechain==3)/chainlength
+
+
+###### extra
+ # dwarf galaxies kinematics from aia DR2
+ # Table D.2
+# https://www.astro.rug.nl/~ahelmi/research/dr2-dggc/
+ 
+dwarfs = read.csv(file = "../../../../Research/Data-tracers/Data/GaiaDR2_DGs/d2.csv")
+
+apos = dwarfs$apocentre
+
+pdf("extra-question_eadie.pdf")
+
+plot(density(apos), main = "Density of DGs' apocenters", xlab = "kpc")
+
+# does not appear normally distributed
+
+# number of samples in data
+n = length(apos)
+
+mean(apos)
+var(apos)
+
+# look at the empirical distribution of the apocenters
+plot(ecdf(x = apos))
+
+# draw n samples with replacement
+samps = sample(x = apos, size = n, replace = TRUE)
+
+sampsmu = mean(samps)
+sampsvar = var(samps)
+
+# make a data frame
+df = data.frame(sampsmu, sampsvar)
+
+# do this over and over to get 1000 means and variances
+for(i in 2:1000){
+
+  samps = sample(x = apos, size = n, replace = TRUE)
+  df[i, "sampsmu"] = mean(samps)
+  df[i, "sampsvar"] = var(samps)
+  
+}
+
+# plot the distribution of the means and variances
+plot(density(df$sampsmu), main="Distribution of Sample Means")
+plot(density(df$sampsvar), main="Distribution of Sample Variances")
+
+# distribution of the means looks almost normal
+# distribution of the variances do no look normal
+
+dev.off()
